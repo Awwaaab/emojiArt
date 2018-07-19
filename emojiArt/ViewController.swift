ae9a1c0b2e0d434d209ff390b31ead449030e7fb
Awab Aly-mac <awab-mac@awabs-imac.local>
2018-07-19 18:37:46 +0200

    Initial Commit

---
+++ emojiArt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emojiArt
+//
+//  Created by Awab Aly-mac on 19/7/18.
+//  Copyright © 2018 Awab Aly-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
